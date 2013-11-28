db38e71f972590ee3ed0cb8d4bc8e0bddddbea8a
Jason H Weitzel <jsnweitzel@gmail.com>
2013-11-28 07:56:04 -0500

    strict-aliasing fix 	modified:   common.mk

--- common.mk
+++ common.mk
@@ -14,7 +14,7 @@ common_header_export_path := qcom/display
 common_libs := liblog libutils libcutils libhardware
 
 #Common C flags
-common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers -Wno-attributes
+common_flags := -DDEBUG_CALC_FPS -Wno-missing-field-initializers -Wno-attributes -Wno-strict-aliasing
 
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
     common_flags += -D__ARM_HAVE_NEON
